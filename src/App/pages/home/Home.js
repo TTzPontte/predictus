470a9c703bb49fb023b85310852e6cecfd0e0e4b
Matheus Duarte <matheusc.d@hotmail.com>
2023-04-19 17:56:38 -0300

    feat: firt commit

---
+++ src/App/pages/home/Home.js
@@ -0,0 +1,20 @@
+import HomePage from '../../components/HomePage';
+import {useForm, FormProvider} from "react-hook-form";
+import Input from "../../components/form/input/Input"
+import { Row, Col, Button } from 'react-bootstrap';
+
+const Home = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+   
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName", { required: true, maxLength: 20 })} />
+      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input type="number" {...register("age", { min: 18, max: 99 })} />
+      <input type="submit" />
+    </form>
+  );
+};
+export default Home;
+
