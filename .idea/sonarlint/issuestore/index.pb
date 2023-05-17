01062ffcba1648ad9efb6f81d153a166317368fa
Mr-i-me <Mr-i-me@Ivans-MacBook-Pro.local>
2023-05-17 01:52:47 -0300

    v5

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,4 @@
 
-L
-src/App/servicer/gerToken.js,e/8/e8314e26e0027133ac22f635feadb00ffc84de2b
-K
-src/App/servicer/Handler.js,a/9/a955fcd90744b1fde7a5f1b0351ea82683f9152a
 <
 src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
 X
@@ -14,15 +10,5 @@ K
 src/App/Pages/Home/index.js,9/6/96382e339cf9476cb8e0f30e99eb2ad8dbbea8d8
 C
 src/App/App.test.js,3/0/307dcfbdee9926a2948dc6e2676d85cc01583bdb
-X
-(src/App/Pages/Home/FirstPage/styles.scss,0/2/02ceadd36fe0dfea462c5f0d1aab13088b7ffd8c
 N
 src/App/Pages/Home/styles.scss,c/8/c845af6d96dbb1abffb252c717d2455416d4f850
-b
-2src/App/Pages/Home/SecondPage/tables/fileTables.js,c/3/c30ce306ccfd539cf76b887b8d504f6ce5d956ab
-a
-1src/App/Pages/Home/SecondPage/tables/BankTable.js,0/f/0fcec95042cd836226eaaaa900d6dd5c30c2448b
-T
-$src/App/Pages/Home/FirstPage/Help.js,c/7/c7681a98027a6ee62ad353742c88d988c266ff80
-[
-+src/App/Pages/Home/FirstPage/FirstPage__.js,6/d/6d050a27493e2036ed630ef48f220810012a5de3
--- src/App/Pages/Home/Pages/Home.js
+++ src/App/Pages/Home/Pages/Home.js
@@ -1,114 +1,137 @@
-import React, { useState } from "react";
-import { Helmet } from "react-helmet";
-import { Button, Col, Row } from "react-bootstrap";
-import { uploadFileToS3, usePdfConversionAndMerge } from "../helpers";
-import { pdfjs } from "react-pdf";
+import React, {useState} from "react";
+import {Helmet} from "react-helmet";
+import {Button, Card, Col, Row} from "react-bootstrap";
+import {uploadFileToS3, usePdfConversionAndMerge} from "../helpers";
+import {pdfjs} from "react-pdf";
 import "../styles.scss";
 import FirstPage from "./FirstPage/FirstPage";
-import { Storage } from "@aws-amplify/storage";
-
+import {Storage} from "@aws-amplify/storage";
 // Set the worker URL for PDF.js
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const PdfToImageActions = ({ mergedImageSrc, pdfFileName }) => {
-  const downloadMergedPdf = async () => {
-    if (!mergedImageSrc) {
-      return;
-    }
+const PdfToImageActions = ({mergedImageSrc, pdfFileName}) => {
+    const downloadMergedPdf = async () => {
+        if (!mergedImageSrc) {
+            return;
+        }
 
-    try {
-      const response = await fetch(mergedImageSrc);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+        try {
+            const response = await fetch(mergedImageSrc);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
 
-      const blob = await response.blob();
-      const fileWithoutExtension = pdfFileName.slice(0, pdfFileName.lastIndexOf("."));
-      const fileName = `${fileWithoutExtension}.jpg`;
-      const fileKey = await uploadFileToS3(fileName, blob, "image/jpeg");
-      console.log("fileKey", fileKey);
-    } catch (error) {
-      console.error("Error downloading merged image:", error);
-    }
-  };
+            const blob = await response.blob();
+            const fileWithoutExtension = pdfFileName.slice(0, pdfFileName.lastIndexOf("."));
+            const fileName = `${fileWithoutExtension}.jpg`;
+            const fileKey = await uploadFileToS3(fileName, blob, "image/jpeg");
+            console.log("fileKey", fileKey);
+        } catch (error) {
+            console.error("Error downloading merged image:", error);
+        }
+    };
 
-  return (
-      <Row className="my-4">
-        <Col>
-          <Button onClick={downloadMergedPdf} disabled={!mergedImageSrc}>
-            Enviar Imagem
-          </Button>
-        </Col>
-      </Row>
-  );
+    return (
+        <Row className="my-4">
+            <Col>
+                <Button onClick={downloadMergedPdf} disabled={!mergedImageSrc}>
+                    Enviar Imagem
+                </Button>
+            </Col>
+        </Row>
+    );
 };
 
 const Ofx = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const pdfFile = selectedFile?.statement?.fileData;
-  const mergedImageSrc = usePdfConversionAndMerge(pdfFile);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const pdfFile = selectedFile?.statement?.fileData;
+    const mergedImageSrc = usePdfConversionAndMerge(pdfFile);
 
-  const handleUpload = async ({ fileName, fileData, fileType }) => {
-    try {
-      const fileKey = await uploadFileToS3(fileName, fileData, fileType);
-      setSelectedFile((prevState) => ({
-        ...prevState,
-        statement: {
-          ...prevState?.statement,
-          s3Key: fileKey,
-          transactions: [],
-          monthlyTransactions: [],
-          fileData,
-          fileName, // add fileName to the state
-        },
-      }));
-    } catch (error) {
-      console.error("Error handling upload:", error);
-    }
-  };
+    const handleUpload = async ({fileName, fileData, fileType}) => {
+        try {
+            const fileKey = await uploadFileToS3(fileName, fileData, fileType);
+            setSelectedFile((prevState) => ({
+                ...prevState,
+                statement: {
+                    ...prevState?.statement,
+                    s3Key: fileKey,
+                    transactions: [],
+                    monthlyTransactions: [],
+                    fileData,
+                    fileName, // add fileName to the state
+                },
+            }));
+        } catch (error) {
+            console.error("Error handling upload:", error);
+        }
+    };
 
-  const callApi = () => {
-    fetch("https://1qe2rpg7yb.execute-api.us-east-1.amazonaws.com/Prod/hello/")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.error("Error calling API:", error);
-        });
-  };
+    const [reportUrl, setReportUrl] = useState()
+    const callApi = async () => {
+        try {
+            const pdfFileName = selectedFile?.statement?.fileName;
+            const fileWithoutExtension = pdfFileName.slice(0, pdfFileName.lastIndexOf("."));
 
-  return (
-      <div className="ofx">
-        <Helmet>
-          <title>Ofx</title>
-          <meta name="description" content="Ofx" />
-        </Helmet>
-        <div className="container">
-          <div className="contractPage">
-            <div className="contractPage--header" style={{ padding: "0 1em" }}>
-              <h1>Aferição de renda</h1>
-              <br />
-              <Button onClick={callApi}>Call API</Button>
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify({ file: fileWithoutExtension })
+            };
+
+            const response = await fetch("https://eovdxkiwg4u4s8v.m.pipedream.net", requestOptions);
+
+            const storageData = await Storage.get('test.txt', {
+                level: 'public'
+            });
+            setReportUrl(storageData)
+            console.log(storageData);
+        } catch (error) {
+            console.error("Error calling API:", error);
+        }
+    };
+
+    return (
+        <div className="ofx">
+            <Helmet>
+                <title>Ofx</title>
+                <meta name="description" content="Ofx"/>
+            </Helmet>
+            <div className="container">
+                <div className="contractPage">
+                    <div className="contractPage--header" style={{padding: "0 1em"}}>
+                        <h1>Aferição de renda</h1>
+                        <br/>
+                        <Button onClick={callApi}>Call API</Button>
+                    </div>
+                    <hr/>
+                    <div className="react-tabs__tab-panel react-tabs__tab-panel--selected">
+                        {!mergedImageSrc && <FirstPage handleUpload={handleUpload}/>}
+                        {mergedImageSrc && (
+                            <>
+                                <Row>
+                                    <Col>
+                                        <Card>
+
+                                            <embed src={mergedImageSrc} type="application/pdf" width="100%"
+                                                   height="600px"/>
+                                        </Card>
+                                    </Col>
+                                </Row>
+                                <PdfToImageActions mergedImageSrc={mergedImageSrc}
+                                                   pdfFileName={selectedFile?.statement?.fileName}/>
+                            </>
+                        )}
+                    </div>
+                    {!!reportUrl && <h1>{reportUrl}</h1>}
+                </div>
             </div>
-            <hr />
-            <div className="react-tabs__tab-panel react-tabs__tab-panel--selected">
-              {!mergedImageSrc && <FirstPage handleUpload={handleUpload} />}
-              {mergedImageSrc && (
-                  <>
-                    <Row>
-                      <Col>
-                        <embed src={mergedImageSrc} type="application/pdf" width="100%" height="600px" />
-                      </Col>
-                    </Row>
-                    <PdfToImageActions mergedImageSrc={mergedImageSrc} pdfFileName={selectedFile?.statement?.fileName} />
-                  </>
-              )}
-            </div>
-          </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default Ofx;
+
+
