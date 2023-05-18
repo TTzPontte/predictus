0889d57168aa42f84fcb66421a31739d0447712f
Mr-i-me <Mr-i-me@Ivans-MacBook-Pro.local>
2023-05-18 10:48:03 -0300

    v25

--- src/App/Pages/Home/Pages/Home.js
+++ src/App/Pages/Home/Pages/Home.js
@@ -1,95 +1,109 @@
-import React, { useState } from "react";
-import { Helmet } from "react-helmet";
-import { Button } from "react-bootstrap";
-import { downloadFromS3, uploadFileToS3 } from "../helpers";
-import { pdfjs } from "react-pdf";
+import React, {useState} from "react";
+import {Helmet} from "react-helmet";
+import {Button} from "react-bootstrap";
+import {downloadFromS3, uploadFileToS3} from "../helpers";
+import {pdfjs} from "react-pdf";
 import "../styles.scss";
 import FirstPage from "./FirstPage/FirstPage";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-const ApiCaller = ({ selectedFile, selectedFileName, downloadReport }) => {
-  const [reportUrl, setReportUrl] = useState(null);
-  const callApi = async () => {
-    try {
-      console.log({ selectedFile });
-      const fileWithoutExtension = selectedFile.name.slice(0, selectedFile.name.lastIndexOf("."));
 
-      const doIt = async () => {
-        const headers = new Headers();
+const OpenNewTabButton = ({url}) => {
+    const openNewTab = () => {
+        window.open(url);
+    };
 
-        const options = {
-          method: "POST",
-          headers,
-          // mode: "cors",
-          body: JSON.stringify({ file_name: fileWithoutExtension })
-        };
-        const url = "https://eoyg4htk97xza02.m.pipedream.net";
-        const response = await fetch(url, options);
+    return <Button onClick={openNewTab}>Open New Tab</Button>;
+};
+const delay = (callback, timeout) => {
+    setTimeout(callback, timeout);
+};
+const ApiCaller = ({selectedFile, selectedFileName}) => {
+    const [reportUrl, setReportUrl] = useState(null);
 
-        console.log({ response });
-      };
-      await doIt();
-      await downloadFromS3(selectedFileName);
-    } catch (error) {
-      console.error("Error calling API:", error);
-    }
-  };
+    const callApi = async () => {
+        try {
+            const fileWithoutExtension = selectedFileName.slice(0, selectedFileName.lastIndexOf("."));
 
-  return (
-    <div>
-      <Button onClick={callApi}>Call API</Button>
-      {reportUrl && <h1>{reportUrl}</h1>}
-    </div>
-  );
+            const fetchApi = async () => {
+                const headers = new Headers();
+                const options = {
+                    method: "POST",
+                    headers,
+                    body: JSON.stringify({file_name: fileWithoutExtension})
+                };
+                const url = "https://eoyg4htk97xza02.m.pipedream.net";
+                // const url = "https://v6v4s76qhmeqzlex7ial2kmtxi0hriio.lambda-url.us-east-1.on.aws/";
+                const response = await fetch(url, options);
+                console.log({response});
+            };
+
+            const getFILE = async () => {
+                const presignedUrl = await downloadFromS3(selectedFileName);
+                await setReportUrl(presignedUrl);
+            }
+            await fetchApi();
+            await delay(getFILE, 200)
+
+        } catch (error) {
+            console.error("Error calling API:", error);
+        }
+    };
+
+    return (
+        <div>
+            <Button onClick={callApi}>Call API</Button>
+            {reportUrl && <OpenNewTabButton url={reportUrl}/>}
+        </div>
+    );
 };
 
 const Ofx = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleUpload = async ({ fileName, fileData, fileType }) => {
-    try {
-      const fileKey = await uploadFileToS3(fileName, fileData, fileType);
-
-      setSelectedFile((prevState) => ({
-        ...prevState,
-        statement: {
-          ...prevState?.statement,
-          s3Key: fileKey,
-          transactions: [],
-          monthlyTransactions: [],
-          fileData,
-          fileName // add fileName to the state
+    const handleUpload = async ({fileName, fileData, fileType}) => {
+        try {
+            const fileKey = await uploadFileToS3(fileName, fileData, fileType);
+            const fileStatement = {
+                s3Key: fileKey,
+                transactions: [],
+                monthlyTransactions: [],
+                fileData,
+                fileName // add fileName to the state
+            };
+            setSelectedFile({statement: fileStatement});
+        } catch (error) {
+            console.error("Error handling upload:", error);
         }
-      }));
-    } catch (error) {
-      console.error("Error handling upload:", error);
-    }
-  };
+    };
 
-  return (
-    <div className="ofx">
-      <Helmet>
-        <title>Ofx</title>
-        <meta name="description" content="Ofx" />
-      </Helmet>
-      <div className="container">
-        <div className="contractPage">
-          <div className="contractPage--header" style={{ padding: "0 1em" }}>
-            <h1>Aferição de renda</h1>
-            <br />
-          </div>
-          <hr />
+    return (
+        <div className="ofx">
+            <Helmet>
+                <title>Ofx</title>
+                <meta name="description" content="Ofx"/>
+            </Helmet>
+            <div className="container">
+                <div className="contractPage">
+                    <div className="contractPage--header" style={{padding: "0 1em"}}>
+                        <h1>Aferição de renda</h1>
+                        <br/>
+                    </div>
+                    <hr/>
 
-          <div className="react-tabs__tab-panel react-tabs__tab-panel--selected">
-            <FirstPage handleUpload={handleUpload} />
-            {selectedFile?.statement?.fileData && (
-              <ApiCaller selectedFile={selectedFile?.statement?.fileData} selectedFileName={selectedFile?.statement?.fileName}/>
-            )}
-          </div>
+                    <div className="react-tabs__tab-panel react-tabs__tab-panel--selected">
+                        <FirstPage handleUpload={handleUpload}/>
+                        {selectedFile?.statement?.fileData && (
+                            <ApiCaller
+                                selectedFile={selectedFile.statement.fileData}
+                                selectedFileName={selectedFile.statement.fileName}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Ofx;
